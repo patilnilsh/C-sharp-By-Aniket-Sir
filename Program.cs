8b6d7b9c59c8690ba0c65e9c329f5f86944f8511
patilnilsh <105921055+patilnilsh@users.noreply.github.com>
2022-09-23 14:30:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace AdvacedCSharp
+{
+   
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
